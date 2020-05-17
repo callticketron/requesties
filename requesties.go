8f310452c1bacf485e85352de5a943aa7aea0e45
Ticketron <ticketron@protonmail.com>
2020-05-17 18:41:28 -0500

    Initial something-or-other

--- requesties.go
+++ requesties.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	response string
+}
+
+func Get(url string) *Response {
+	resp, err := http.Get("https://www.google.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bodystring := string(body)
+	return &Response{response: bodystring}
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	a := Get("http://www.google.com")
+	fmt.Println(a.response)
 }
